e91af193fa35864b1732176bf3c251d3f2008056
shahbaz8 <muhammadshahbaz9934@gmail.com>
2018-01-07 14:04:44 +0500

    test changes

---
+++ app/src/main/java/com/example/itcomputerszone/gsonmaster/MainActivity.java
@@ -0,0 +1,30 @@
+com.itcomputerszone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    Gson gson;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this is my test
+        setContentView(R.layout.activity_main);
+        t1= (TextView) findViewById(R.id.TextView);
+        gson = new Gson();
+
+        Contact c = new Contact(1,"Salman",true);
+        String str=gson.toJson(c);
+
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+
+        intent.putExtra("MyObjectString",str);
+        startActivity(intent);
+        //shahbaz
+}
